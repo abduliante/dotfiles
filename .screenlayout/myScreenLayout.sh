778088613d1547ff28013285faa1364730036e42
abdulium <aalrubaiya@gmail.com>
2021-05-31 00:40:47 +0300

    first commit

---
+++ .screenlayout/myScreenLayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 1920x1080 --rate 120 --pos 2560x180 --rotate normal --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --primary --scale 1x1 --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
